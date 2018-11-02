2fef3cc6e870b616df786279f0d2e54495431bfa
garmart13 <nayelly.garzon@gmail.com>
2018-11-02 09:44:45 -0600

    Recibir datos de otras apps

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -16,12 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Actividad 5 Recibir datos de otras aplicaciones
-        //https://developer.android.com/training/sharing/receive
-        //Las aplicaciones en android pueden recibir informacion de otras apps
 
-        // El archivo del layout de la actividad se encuentra en la carpeta
-        // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
 
@@ -32,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText et = findViewById(R.id.et);
 
 
-        //Cuando nuestra aplicacion sea iniciada por otra  aplicacion, type no sera nulo
+
         if (Intent.ACTION_SEND.equals(action) && type != null) {
 
             String extraText = intent.getStringExtra(Intent.EXTRA_TEXT);
@@ -41,18 +36,5 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-
-
-        //Actividades
-        //a) Descomenta las lineas del archivo AndroidManifest.xml
-        //b) Revisa la url de referencia, que tipo de informacion puede recibir la app?
-        //c) Ejecuta la aplicacion en modo de depuracion, la primera vez que se ejecuta la app, que valor tiene Intent.ACTION_SEND?
-        //d) Prueba compartiendo un video de YouTube o una ubicacion de GoogleMaps a la aplicacion
-        //e) Cuando la aplicacion es iniciada por otra app, que valor tiene Intent.ACTION_SEND?
-        //f) Anota tus respuestas en un documento en Word
-        //g) Sube tu codigo al repositorio.
-        //h) Sube el documento Word con tus respuestas y las capturas de pantalla  a la plataforma Moodle. Incluye la liga a tu repositorio
-
-
     }
 }
